3b0f25f2151841e09bebdae39b2e0454cab434d4
ivelizrodriguez <ivelizdeflores@gmail.com>
2024-09-26 08:38:30 -0500

    Carpetas con programas de conjuntos y comprehensions

---
+++ Conjuntos/main.py
@@ -0,0 +1,10 @@
+countries = {"MX", "COL", "ARG", "USA"}
+northAm = {"USA", "CANADA"}
+centralAm = {"MX", "GT", "BZ"}
+southAm = {"COL", "BZ", "ARG"}
+
+new_set = set()
+
+countries = {"MX", "COL", "ARG", "USA", "USA", "CANADA", "MX", "GT", "BZ", "COL", "BZ", "ARG"}
+set_countries = set(countries)
+print(set_countries)
